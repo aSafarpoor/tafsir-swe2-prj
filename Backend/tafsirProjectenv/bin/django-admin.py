fd1dae5db88a2fd92870533af9d6d71fbf50b569
Your Name <amirhossein.lmpd@yahoo.com>
2019-03-19 10:40:40 +0330

    Project Skeleton

---
+++ Backend/tafsirProjectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/snoop/Desktop/UNI/term 6/TafsirProject/Backend/tafsirProjectenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
