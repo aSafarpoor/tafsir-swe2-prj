0507e773b7a6a3ae868239f19603bc4a9d301e22
Ali <aleyasin10@gmail.com>
2019-05-15 10:21:21 +0430

    change settings

--- Backend/TafsirProject/settings.py
+++ Backend/TafsirProject/settings.py
@@ -43,10 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
-
-    'corsheaders.middleware.CorsMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,7 +117,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
