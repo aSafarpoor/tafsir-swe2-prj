6bf7bb94945fe12ef42b8fd9a5dab32c7c750085
amir-amini <amirhossein.lmpd@yahoo.com>
2019-06-06 22:55:07 +0430

    send to DB video v2

--- Backend/aparat/VideoSend/urls.py
+++ Backend/aparat/VideoSend/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 from django.contrib import admin
+from VideoSend import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'aparat.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    path('postvideo/', admin.site.urls),
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>', views.snippet_detail),
 
 
 ]
